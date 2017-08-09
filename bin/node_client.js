4e3c15d98b5d2612f9720f725865298ed236c9ab
Ethan Riley <ethanmriley@gmail.com>
2017-08-09 13:14:52 -0400

    1.2.0
    Added support for multiple browser WebSocket connections to one node_client instance.

--- bin/node_client.js
+++ bin/node_client.js
@@ -1,6 +1,6 @@
 var SerialPort = require('serialport');
 var WebSocket = require('ws');
-var socket = new WebSocket.Server({host: '127.0.0.1', port: 8080});
+var wss = new WebSocket.Server({host: '127.0.0.1', port: 8080});
 var manual = false;
 
 if(process.argv[2] == "-p" || process.argv[2] == "--port") {
@@ -14,60 +14,62 @@ if(process.argv[2] == "-p" || process.argv[2] == "--port") {
 }
 
 
-socket.on('connection', function(connection) {
+wss.on('connection', function(connection) {
 	console.log("WebSocket connection established.");
-	connection.send(45);
-	initDataStream(streamFromSerial, connection);
+	connection.send(9.99);
+	if (wss.clients.size === 1) {
+		initArduinoConnection();
+	}
 });
 
-function initDataStream(openSerialConn, ws_connection) {
-
-	var portName = "";
-
-	if(process.argv[2] && (process.argv[2] == "-p" || process.argv[2] == "--port")) {
-		portName = process.argv[3];
-		streamFromSerial(portName, ws_connection);
-	} else {
-		SerialPort.list(function(err, ports) {
-			ports.forEach(function(port) {
-				if(port.manufacturer && port.manufacturer.indexOf("Arduino") !== -1) { //connect to the first device we see that has "Arduino" in the manufacturer name
-					portName = port.comName;
-					streamFromSerial(portName, ws_connection);
-				}
-			});
-		});
-	}
-
-}
-
-function streamFromSerial(port_name, ws_connection) {
-
-	var Arduino = new SerialPort(port_name, {
-		parser: SerialPort.parsers.readline('\r\n') //'\r\n' is the spacing character added by Arduino's Serial.println() function, so we use it to parse data
-	});
-
-	Arduino.on('open', function() {
-		console.log("Port " + port_name + " opened.");
-	});
-
-	Arduino.on('data', function(data) {
-		if (ws_connection.readyState === 1) {   
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function sendData(client) {
+		if (client.readyState === 1) {   
 			try {
-				ws_connection.send(data);
+				client.send(data);
 			} catch(err) {
 				console.log(err);
 			}
 		} else {
-			console.log("Websocket connection is not open.");
-			ws_connection.close();
-			if (Arduino.isOpen()) {
-				Arduino.flush();
-				Arduino.close(); 
-			}
+			client.terminate();
 		}
+	});
+};
+
+var streamFromSerial = function streamFromSerial(portName) {
+
+	var Arduino = new SerialPort(portName, {
+		parser: SerialPort.parsers.readline('\r\n') //'\r\n' is the spacing character added by Arduino's Serial.println() function, so we use it to parse data
+	});
+
+	Arduino.on('open', function() {
+		console.log("Port " + portName + " opened.");
+	});
+
+	Arduino.on('data', function(data) {
+		wss.broadcast(data.toString());
 	});
 
 	Arduino.on('error', function(err) {
 		console.log(err);
 	});
+}
+
+function initArduinoConnection() {
+
+	var portName = "";
+
+	if(process.argv[2] && (process.argv[2] == "-p" || process.argv[2] == "--port")) {
+		portName = process.argv[3];
+		streamFromSerial(portName);
+	} else {
+		SerialPort.list(function(err, ports) {
+			ports.forEach(function(port) {
+				if(port.manufacturer && port.manufacturer.indexOf("Arduino") !== -1) { //connect to the first device we see that has "Arduino" in the manufacturer name
+					portName = port.comName;
+					streamFromSerial(portName);
+				}
+			});
+		});
+	}
 }
