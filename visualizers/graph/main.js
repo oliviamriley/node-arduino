1932dbc1ac6223ed34e163b37fca6d7be185c131
IMCJuryd <xuy11@rpi.edu>
2017-11-01 09:18:50 -0400

    new features
    change to meet eslint style and add a toggle feature

--- visualizers/graph/main.js
+++ visualizers/graph/main.js
@@ -1,128 +1,204 @@
-var data = [[]],
-	refresh = {},
-	terminalDisplay = {},
-	autoscroll = true,
-	layout = {},
-	socket = {},
-	time = new Date(),
-	initial_time = time.getTime();
+let data = [[]];
+let refresh = {};
+let terminalDisplay = {};
+let autoscroll = true;
+let layout = {};
+let socket = {};
+let time = new Date();
+let InitialTime = time.getTime();
+
+let sentData = 0;
 
 layout = {
-  title:'node-arduino',
+  title: 'node-arduino',
   height: 400,
   width: 480,
   xaxis: {
-  	showticklabels: false,
+      showticklabels: false,
   },
-  showlegend: true
+  showlegend: true,
 };
 
-//setup empty graph (for cosmetic UI purposes)
+// setup empty graph (for cosmetic UI purposes)
 Plotly.newPlot('myChart', data, layout, {displayModeBar: false});
 
-terminalDisplay = document.getElementById("terminalDisplay");
+terminalDisplay = document.getElementById('terminalDisplay');
 
+/**
+ * Scroll display
+ * @param {String} div - Display Channel to be scrolled
+ */
 function scroll(div) {
-	if (autoscroll) {
-		$('#' + div).animate({
-			scrollTop: $('#' + div)[0].scrollHeight
-		}, 800);
-	}
+    if (autoscroll) {
+        $('#' + div).animate({
+            scrollTop: $('#' + div)[0].scrollHeight,
+        }, 800);
+    }
 }
 
-//disable auto-scrolling on button click, scroll to bottom immediately when enabled
-$("#toggleScroll").click(function() {
-	autoscroll = !autoscroll;
-	scroll("terminalDisplay");
+/**
+ * disable auto-scrolling on button click
+ * scroll to bottom immediately when enabled
+ */
+$('#toggleScroll').click(function() {
+    autoscroll = !autoscroll;
+    scroll('terminalDisplay');
 });
 
 
-socket = new WebSocket("ws://127.0.0.1:8080"); //attempt WebSocket connection with localhost on page load
+socket = new WebSocket('ws://127.0.0.1:8080'); // attempt WebSocket connection with localhost on page load
 socket.onmessage = function receiveMessage(event) {
-	var new_data = {},
-	entry = {};
+    let NewData = {};
+    let entry = {};
+    entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(event.data));
+    // add new data point to scrolling terminalDisplay div
+    terminalDisplay.appendChild(entry);
+    scroll('terminalDisplay');
+    NewData = parseMessage(event.data);
+    /**
+     * sanity check; 
+     * does the data received have an actual value
+     * or is it just whitespace (common on startup)
+     */
+    if (NewData.value.trim().length > 0) {
+        addData(NewData);
+    }
+    console.log(event.data);
+};
 
-	entry = document.createElement('li');
-	entry.appendChild(document.createTextNode(event.data));
-	terminalDisplay.appendChild(entry);        //add new data point to scrolling terminalDisplay div
-
-	scroll("terminalDisplay");
-
-	new_data = parseMessage(event.data);
-	if (new_data.value.trim().length > 0) {    //sanity check; does the data received have an actual value, or is it just whitespace (common on startup)
-		addData(new_data);
-	}
-
-	console.log(event.data);
-}
-
+/**
+ * Parse message to meet format
+ * @param {string} message - message from server
+ * @return {dictionary} data - break message to a header&value format
+ */
 function parseMessage(message) {
-	var end_of_header = 0,
-		header_ = "",
-		value_ = "",
-		data = {};
+    let EndOfHeader = 0;
+    let header_ = '';
+    let value_ = '';
+    let data = {};
+    EndOfHeader = message.indexOf(':');
 
-	end_of_header = message.indexOf(":");
-
-	if (end_of_header > -1) {                                   //if ":" isn't present, end_of_header === -1
-		header_ = message.substr(0, end_of_header);
-	}
-
-	value_ = message.substr(end_of_header + 1, message.length); //assuming data sent in the format "header: value"
-
-	data = {
-		header: header_,
-		value: value_
-	};
-
-	return data;
+    if (EndOfHeader > -1) { // if ":" isn't present, EndOfHeader === -1
+        header_ = message.substr(0, EndOfHeader);
+    }
+    // assuming data sent in the format "header: value"
+    value_ = message.substr(EndOfHeader + 1, message.length);
+    data = {
+        header: header_,
+        value: value_,
+    };
+    return data;
 }
 
-function addData(new_data) {
-	var dataset = [],
-		is_new_data = true,
-		new_trace = {},
-		new_time =  new Date(),
-		time_on_add = new_time.getTime() - initial_time,
-		i = 0;
-
-	for(i = 0; i < data.length; i++) {
-		dataset = data[i];
-		if (dataset.header === new_data.header) { //if the header on the message matches any existing dataset, add the message's data to that dataset
-			dataset.x.push(time_on_add);          //x-axis is time by default
-			dataset.y.push(new_data.value);
-			is_new_data = false;
-		}
-	}
-
-	if(is_new_data) {                 //if the message has an unknown header, create a new dataset, add the message data to it, and redraw the chart
-		new_trace = {
-			name: new_data.header,    //displayed in the legend; this is intended to be customizable for the users through the GUI
-			header: new_data.header,  //pulled right from the message; used for identifying datasets
-			x: [time_on_add],
-			y: [new_data.value],
-			mode: 'lines'
-		};
-		if(new_data.header === "") {  //just for cosmetic purposes; if we recieve data without a header, give it some default name
-			new_trace.name = "data";
-		}
-
-		data.push(new_trace);
-		Plotly.newPlot('myChart', data, layout, {displayModeBar: false}); //newPlot() adds new traces to the graph and takes longer
-
-	} else {
-		Plotly.update('myChart', data, layout, {displayModeBar: false});  //update() will only add to existing data, but is quicker
-	}
+/**
+ * Add Data to the dataset 
+ * @param {string} NewData - the new data collected 
+ */
+function addData(NewData) {
+    /*
+    var dataset = [],
+        IsNewData = true,
+        NewTrace = {},
+        NewTime = new Date(),
+        TimeOnAdd = NewTime.getTime() - InitialTime,
+        i = 0;
+    */
+    let dataset = [];
+    let IsNewData = true;
+    let NewTrace = {};
+    let NewTime = new Date();
+    let TimeOnAdd = NewTime.getTime() - InitialTime;
+    let i = 0;
+    /*
+    const dataset = [];
+    const IsNewData = true;
+    const NewTrace = {};
+    const NewTime = new Date();
+    const TimeOnAdd = NewTime.getTime() - InitialTime;
+    const i = 0;
+    */
+    /*
+    var dataset = [];
+    var IsNewData = true;
+    var NewTrace = {};
+    var NewTime = new Date();
+    var TimeOnAdd = NewTime.getTime() - InitialTime;
+    var i = 0;
+    */
+    for (i = 0; i < data.length; i++) {
+        dataset = data[i];
+        /** 
+         * if the header on the message matches any existing dataset
+         * add the message's data to that dataset
+         */
+        if (dataset.header === NewData.header) {
+            dataset.x.push(TimeOnAdd); // x-axis is time by default
+            dataset.y.push(NewData.value);
+            IsNewData = false;
+        }
+    }
+    /**
+     * if the message has an unknown header
+     * create a new dataset, add the message data to it, and redraw the chart
+     */
+    if (IsNewData) {
+        NewTrace = {
+            /**
+             * name: displayed in the legend; 
+             * this is intended to be customizable for the users through the GUI
+             */
+            name: NewData.header,
+            /**
+             * header: pulled right from the message; 
+             * used for identifying datasets
+             */
+            header: NewData.header,
+            x: [TimeOnAdd],
+            y: [NewData.value],
+            mode: 'lines',
+        };
+        /**
+         * NewData: just for cosmetic purposes; 
+         * if we recieve data without a header, give it some default name 
+         */
+        if (NewData.header === '') {
+            NewTrace.name = 'data';
+        }
+        data.push(NewTrace);
+        /** newPlot() adds new traces to the graph and takes longer */
+        Plotly.newPlot('myChart', data, layout, {displayModeBar: false});
+    } else {
+        /** update() will only add to existing data, but is quicker */
+        Plotly.update('myChart', data, layout, {displayModeBar: false});
+    }
 }
 
-refresh = document.getElementById("refresh");
-refresh.onclick = function(){
-	location.reload();
-}
+// socket_s = new WebSocket("ws://127.0.0.1:8081"); 
 
-window.onbeforeunload = closeConnection; //close websocket on browser close or refresh
+toggle = document.getElementById('toggle');
+toggle.onclick = function() {
+    if (sentData == 0) {
+        sentData = 1;
+    } else {
+        sentData = 0;
+    }
+    // sentData +=1;
 
+    socket.send(sentData.toString());
+    console.log(sentData.toString(), 'has been sent');
+};
+
+refresh = document.getElementById('refresh');
+refresh.onclick = function() {
+    location.reload();
+};
+
+// close websocket on browser close or refresh
+window.onbeforeunload = closeConnection;
+
+/** Close websocket connection. */
 function closeConnection() {
-	socket.onclose = function(){};       //disable default handler
-	socket.close()
+    socket.onclose = function() {}; // disable default handler
+    socket.close();
 }
